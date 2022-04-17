0e9ecb4687c2ae327b3a68c759df9c5afbbc8543
qqmikey <qqmikey@icoud.com>
2022-04-17 19:32:04 +0200

    export secure files

--- packages/camera/camera/lib/camera.dart
+++ packages/camera/camera/lib/camera.dart
@@ -17,3 +17,5 @@ export 'package:camera_platform_interface/camera_platform_interface.dart'
 export 'src/camera_controller.dart';
 export 'src/camera_image.dart';
 export 'src/camera_preview.dart';
+export 'src/secure_platform_view.dart';
+export 'src/zoom_focus_point_wrap.dart';
